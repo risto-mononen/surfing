888c8eb54ccfd6f84909d8d40bd9536f30e3d728
Risto Mononen <risto.mononen@notava.com>
2013-04-15 00:33:57 +0300

    Refactor kitchen* to surfing members

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-kitchensink: Assortment of technologies including Arquillian
+Surfing members: Assortment of technologies including Arquillian
 ========================
 Author: Pete Muir
 Level: Intermediate
@@ -14,7 +14,7 @@ This is your project! It is a sample, deployable Maven 3 project to help you get
 
 This project is setup to allow you to create a compliant Java EE 6 application using JSF 2.0, CDI 1.0, EJB 3.1, JPA 2.0 and Bean Validation 1.0. It includes a persistence unit and some sample persistence and transaction code to introduce you to database access in enterprise Java. 
 
-There is a tutorial for this quickstart in the [Getting Started Developing Applications Guide](http://www.jboss.org/jdf/quickstarts/jboss-as-quickstart/guide/KitchensinkQuickstart/).
+There is a tutorial for this quickstart in the [Getting Started Developing Applications Guide](http://www.jboss.org/jdf/quickstarts/jboss-as-quickstart/guide/SurfingMembers/).
 
 System requirements
 -------------------
--- src/main/java/org/surfing/apps/members/model/Member.java
+++ src/main/java/org/surfing/apps/members/model/Member.java
@@ -1,3 +1,19 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.surfing.apps.members.model;
 
 import java.io.Serializable;
@@ -28,12 +44,18 @@ public class Member implements Serializable {
     private Long id;
 
     @NotNull
+    @Pattern(regexp = "[^0-9]*", message = "Must not contain numbers")
     private String name;
 
     @NotNull
     @NotEmpty
     @Email
     private String email;
+
+    @Size(min = 10, max = 12)
+    @Digits(fraction = 0, integer = 12)
+    @Column(name = "phone_number")
+    private String phoneNumber;
 
     public Long getId() {
         return id;
@@ -59,4 +81,11 @@ public class Member implements Serializable {
         this.email = email;
     }
 
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
