de6fa0d534ba3df84a6dda49fbd31b9268e0296d
Risto Mononen <risto.mononen@notava.com>
2013-04-14 20:52:27 +0300

    Relax member field validation and fix initial data

--- src/main/java/org/surfing/apps/members/model/Member.java
+++ src/main/java/org/surfing/apps/members/model/Member.java
@@ -28,8 +28,6 @@ public class Member implements Serializable {
     private Long id;
 
     @NotNull
-    @Size(min = 1, max = 25)
-    @Pattern(regexp = "[^0-9]*", message = "Must not contain numbers")
     private String name;
 
     @NotNull
