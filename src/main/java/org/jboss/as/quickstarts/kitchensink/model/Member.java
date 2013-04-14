2c00cb046cc20606aed66c93779195c6b9ec9c9f
Risto Mononen <risto.mononen@notava.com>
2013-04-15 00:00:07 +0300

    Fix DS name and member validation

--- src/main/java/org/jboss/as/quickstarts/kitchensink/model/Member.java
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/model/Member.java
@@ -44,7 +44,6 @@ public class Member implements Serializable {
     private Long id;
 
     @NotNull
-    @Size(min = 1, max = 25)
     @Pattern(regexp = "[^0-9]*", message = "Must not contain numbers")
     private String name;
 
@@ -53,7 +52,6 @@ public class Member implements Serializable {
     @Email
     private String email;
 
-    @NotNull
     @Size(min = 10, max = 12)
     @Digits(fraction = 0, integer = 12)
     @Column(name = "phone_number")
